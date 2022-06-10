a6d9df5ec1c88a15f9d5b60230e0a2f2cacc14d8
c1500309964 <1500309964@qq.com>
2022-06-10 14:53:29 +0800

    master commit3

--- src/main/java/com/chen/CommunityApplication.java
+++ src/main/java/com/chen/CommunityApplication.java
@@ -13,6 +13,7 @@ public class CommunityApplication {
         SpringApplication.run(CommunityApplication.class, args);
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("master test1!");
     }
 
 }
