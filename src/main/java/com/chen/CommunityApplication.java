3862da217efc74c93e9f7773d3bab5e3a7448fb6
c1500309964 <1500309964@qq.com>
2022-06-10 16:07:48 +0800

    push commit

--- src/main/java/com/chen/CommunityApplication.java
+++ src/main/java/com/chen/CommunityApplication.java
@@ -17,6 +17,7 @@ public class CommunityApplication {
         System.out.println("hello git3");
         System.out.println("hello git4");
         System.out.println("hot-fix test1");
+        System.out.println("push test");
     }
 
 }
